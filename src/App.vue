7a1c03959c1488d233628289f8be8e06a85b1ed8
BQX <b22050106@njupt.edu.cn>
2023-10-24 22:28:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <Show></Show>
+</template>
+<script setup>
+import Show from "./pages/show.vue";
+</script>
+
---
+++ src/components/pageMessage.vue
@@ -0,0 +1,50 @@
+<!-- some-inner-component.vue -->
+<template>
+    <p>page {{pageNum}} is {{ swiperSlide.isActive ? 'active' : 'not active'}}</p>
+</template>
+<script setup>
+import { useSwiperSlide } from 'swiper/vue';
+import {ref, watch} from "vue";
+
+const props = defineProps(
+    {
+        pageNum:Number
+    }
+)
+
+const swiperSlide = useSwiperSlide();
+
+let change = ref(true);
+
+let pageMessage = ref([
+    {
+        pageNumber:1,
+        pageIsActive:false
+    },
+    {
+        pageNumber:2,
+        pageIsActive:false
+    },
+    {
+        pageNumber:3,
+        pageIsActive:false
+    },
+    {
+        pageNumber:4,
+        pageIsActive:false
+    }
+])
+
+watch(swiperSlide, () => {
+    if (swiperSlide.value.isActive) {
+        // console.log(props.pageNum);
+        pageMessage.value[props.pageNum-1] = {
+            pageNumber:props.pageNum,
+            pageIsActive:swiperSlide.value.isActive
+        }
+        // console.log("child emit")
+        const messageString = JSON.stringify(pageMessage.value);
+        localStorage.setItem("message",messageString)
+    }
+});
+</script>
