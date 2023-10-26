21a00501b06f60a40b35ed10c8402960f9e744f0
BQX <b22050106@njupt.edu.cn>
2023-10-26 15:12:35 +0800

    update: README.md(#3)

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ When we slide the page, the window switches to the next page, the navigation bar
 
 I didn't see such a style in the samples on the `swiper.js` website, all the styles on the website have a small circle at the bottom of the navigation bar, not a specific `navigation`, so I implemented this feature myself by using the `api` provided by `swiper.js`.
 
-Look [here](https://bqxbqx.github.io/2023/10/25/%E5%9C%A8-vue-%E4%BD%BF%E7%94%A8-swiper-js-%E5%AE%9E%E7%8E%B0%E9%A1%B5%E9%9D%A2%E7%BB%84%E4%BB%B6%E6%BB%91%E5%8A%A8%E7%89%B9%E6%95%88/)
+To see more details click [here](https://bqxbqx.github.io/2023/10/25/%E5%9C%A8-vue-%E4%BD%BF%E7%94%A8-swiper-js-%E5%AE%9E%E7%8E%B0%E9%A1%B5%E9%9D%A2%E7%BB%84%E4%BB%B6%E6%BB%91%E5%8A%A8%E7%89%B9%E6%95%88/)
--- src/pages/show.vue
+++ src/pages/show.vue
@@ -1,33 +1,56 @@
 <template>
-    <swiper
-            :scrollbar="{hide: false}"
-            :modules="modules"
-            class="mySwiper"
-            @swiper="onSwiper"
-            @slideChange="onSlideChange"
+  <swiper
+    :scrollbar="{ hide: false }"
+    :modules="modules"
+    class="mySwiper"
+    @swiper="onSwiper"
+    @slideChange="onSlideChange"
+  >
+    <swiper-slide><PageMessage :pageNum="1"></PageMessage></swiper-slide>
+    <swiper-slide><PageMessage :pageNum="2"></PageMessage></swiper-slide>
+    <swiper-slide><PageMessage :pageNum="3"></PageMessage></swiper-slide>
+    <swiper-slide><PageMessage :pageNum="4"></PageMessage></swiper-slide>
+  </swiper>
+  <div class="bottomNav">
+    <div
+      class="bottomNavItem partOne"
+      :class="{ active: ActivePageNum === 1 }"
+      @click="isActiveItem(1)"
     >
-        <swiper-slide><PageMessage :pageNum=1></PageMessage></swiper-slide>
-        <swiper-slide><PageMessage :pageNum=2></PageMessage></swiper-slide>
-        <swiper-slide><PageMessage :pageNum=3></PageMessage></swiper-slide>
-        <swiper-slide><PageMessage :pageNum=4></PageMessage></swiper-slide>
-    </swiper>
-    <div class="bottomNav">
-        <div class="bottomNavItem partOne" :class="{ active: ActivePageNum === 1 }" @click="isActiveItem(1)">part 1</div>
-        <div class="bottomNavItem partTwo" :class="{ active: ActivePageNum === 2 }" @click="isActiveItem(2)">part 2</div>
-        <div class="bottomNavItem partThree" :class="{ active: ActivePageNum === 3 }" @click="isActiveItem(3)">part 3</div>
-        <div class="bottomNavItem partFour" :class="{ active: ActivePageNum === 4 }" @click="isActiveItem(4)">part 4</div>
+      part 1
     </div>
+    <div
+      class="bottomNavItem partTwo"
+      :class="{ active: ActivePageNum === 2 }"
+      @click="isActiveItem(2)"
+    >
+      part 2
+    </div>
+    <div
+      class="bottomNavItem partThree"
+      :class="{ active: ActivePageNum === 3 }"
+      @click="isActiveItem(3)"
+    >
+      part 3
+    </div>
+    <div
+      class="bottomNavItem partFour"
+      :class="{ active: ActivePageNum === 4 }"
+      @click="isActiveItem(4)"
+    >
+      part 4
+    </div>
+  </div>
 </template>
 <script setup>
 // Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Scrollbar } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/scrollbar';
-import './show.css'
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Scrollbar } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "./show.css";
 import PageMessage from "../components/pageMessage.vue";
 import { ref } from "vue";
-
 
 let ActivePageNum = ref(1);
 let pageMessage = ref();
@@ -35,24 +58,24 @@ let ActivePage = ref();
 let swiperRef = ref();
 
 const onSwiper = (swiper) => {
-    swiperRef.value = swiper;
+  swiperRef.value = swiper;
 };
 
 const onSlideChange = () => {
-    let messageString ;
-    setTimeout(()=>
-    {
-        messageString = localStorage.getItem('message');
-        pageMessage.value = JSON.parse(messageString);
-        ActivePage.value = pageMessage.value.find(item => item.pageIsActive === true);
-        ActivePageNum.value = ActivePage.value.pageNumber;
-    },0)
+  let messageString;
+  setTimeout(() => {
+    messageString = localStorage.getItem("message");
+    pageMessage.value = JSON.parse(messageString);
+    ActivePage.value = pageMessage.value.find(
+      (item) => item.pageIsActive === true
+    );
+    ActivePageNum.value = ActivePage.value.pageNumber;
+  }, 0);
 };
 
-const modules =  [Scrollbar]
+const modules = [Scrollbar];
 
-function isActiveItem(index){
-    swiperRef.value.slideTo(index-1);
+function isActiveItem(index) {
+  swiperRef.value.slideTo(index - 1);
 }
-
 </script>
